cbc1164a95ce829147de195064aa92c977da074e
oscar-amezquita <oscar.amezquita@globant.com>
2017-06-07 11:43:45 -0500

    Fix errors and clear unused imports

--- app/src/main/java/com/globant/example/mentorapp/view/activities/SplashScreenActivity.java
+++ app/src/main/java/com/globant/example/mentorapp/view/activities/SplashScreenActivity.java
@@ -1,33 +1,27 @@
 package com.globant.example.mentorapp.view.activities;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Window;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.globant.example.mentorapp.R;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set portrait orientation
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_splash);
 
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
 
-
             }
         };
         Timer timer = new Timer();
-        timer.schedule(task, R.integer.splash_screen_duration);
+        timer.schedule(task, getResources().getInteger(R.integer.splash_screen_duration));
     }
 }
