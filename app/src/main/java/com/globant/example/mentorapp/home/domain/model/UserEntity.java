07e1dbf93b25bf882130062a00abf8db4e4dfaa7
MARID044 <danielle.x.mariani.-xd@disney.com>
2017-07-11 15:57:38 -0500

    Delete Expose annotation.

--- app/src/main/java/com/globant/example/mentorapp/home/domain/model/UserEntity.java
+++ app/src/main/java/com/globant/example/mentorapp/home/domain/model/UserEntity.java
@@ -1,56 +1,38 @@
 package com.globant.example.mentorapp.home.domain.model;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class UserEntity {
 
-    @Expose
     private String login;
-    @Expose
     private Integer id;
     @SerializedName("avatar_url")
-    @Expose
     private String avatarUrl;
     @SerializedName("gravatar_id")
-    @Expose
     private String gravatarId;
-    @Expose
     private String url;
     @SerializedName("html_url")
-    @Expose
     private String htmlUrl;
     @SerializedName("followers_url")
-    @Expose
     private String followersUrl;
     @SerializedName("following_url")
-    @Expose
     private String followingUrl;
     @SerializedName("gists_url")
-    @Expose
     private String gistsUrl;
     @SerializedName("starred_url")
-    @Expose
     private String starredUrl;
     @SerializedName("subscriptions_url")
-    @Expose
     private String subscriptionsUrl;
     @SerializedName("organizations_url")
-    @Expose
     private String organizationsUrl;
     @SerializedName("repos_url")
-    @Expose
     private String reposUrl;
     @SerializedName("events_url")
-    @Expose
     private String eventsUrl;
     @SerializedName("received_events_url")
-    @Expose
     private String receivedEventsUrl;
-    @Expose
     private String type;
     @SerializedName("site_admin")
-    @Expose
     private Boolean siteAdmin;
 
     public String getLogin() {
