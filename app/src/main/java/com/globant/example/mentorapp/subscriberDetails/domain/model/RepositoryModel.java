9130dfc30047b1b909c0f50e19b8f48582e3a65d
oscar-amezquita <oscar.amezquita@globant.com>
2017-07-12 16:31:07 -0500

    Add repo reciclerview adapter and repository view model

---
+++ app/src/main/java/com/globant/example/mentorapp/subscriberDetails/domain/model/RepositoryModel.java
@@ -0,0 +1,25 @@
+package com.globant.example.mentorapp.subscriberDetails.domain.model;
+
+/**
+ * Represents a model of repository basic information.
+ * Created by oscar.amezquita on 12/07/2017.
+ */
+
+public class RepositoryModel {
+
+    private final String repoName;
+    private final String repoURL;
+
+    public RepositoryModel(String repoName, String repoURL) {
+        this.repoName = repoName;
+        this.repoURL = repoURL;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public String getRepoURL() {
+        return repoURL;
+    }
+}
---
+++ app/src/main/java/com/globant/example/mentorapp/subscriberDetails/presentation/view/adapter/UserRepositoriesAdapter.java
@@ -0,0 +1,59 @@
+package com.globant.example.mentorapp.subscriberDetails.presentation.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.globant.example.mentorapp.R;
+import com.globant.example.mentorapp.subscriberDetails.domain.model.RepositoryModel;
+
+import java.util.List;
+
+/**
+ * Custom adapter to show user repositories by name and url.
+ * Created by oscar.amezquita on 12/07/2017.
+ */
+
+public class UserRepositoriesAdapter extends RecyclerView.Adapter<UserRepositoriesAdapter.ViewHolder> {
+
+    private List<RepositoryModel> userRepositoriesAdapterList;
+    private Context context;
+
+    public UserRepositoriesAdapter(List<RepositoryModel> userRepositoriesAdapterList, Context context) {
+        this.userRepositoriesAdapterList = userRepositoriesAdapterList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_repository_adapter, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        RepositoryModel model = userRepositoriesAdapterList.get(position);
+        holder.repoNameTextView.setText(model.getRepoName());
+        holder.repoURLTextView.setText(model.getRepoURL());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userRepositoriesAdapterList != null ? userRepositoriesAdapterList.size() : context.getResources().getInteger(R.integer.zero_constant);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView repoNameTextView;
+        private TextView repoURLTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            repoNameTextView = (TextView) itemView.findViewById(R.id.textName);
+            repoURLTextView = (TextView) itemView.findViewById(R.id.textName);
+        }
+    }
+}
